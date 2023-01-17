6d7fcdde43513d43c7c6cb3e1a61cc593a2a9dba
Paul Commerell (paul commerell <129955348+Desponark@users.noreply.github.com>
2023-01-17 17:36:25 +0100

    - implemented more detailed jump and movement

--- Prototype2/assets/player/Player.gd
+++ Prototype2/assets/player/Player.gd
@@ -1,61 +1,80 @@
 extends KinematicBody2D
 
+const upDiretion = Vector2.UP
 
-export var maxSpeed = 500
-export var maxJumpHeight = -500
-export var minJumpHeight = -200
-export var gravity = 1000
-export (float, 0, 1.0) var friction = 0.2
+export var maxSpeed = 600.0
+export var maxJumpHeight = -1500.0
+export var minJumpHeight = -500.0
+export var maximumJumps = 2
+export var doubleJumpHeight = -1200.0
+export var gravity = 4500.0
 export (float, 0, 1.0) var acceleration = 0.1
+export (float, 0, 1.0) var friction = 0.2
 
+var jumpsMade = 0
 var velocity = Vector2.ZERO
 
 export var maxHealth = 100
 var health = 100
-var stateMachine
 
 func _ready():
 	health = maxHealth
-	stateMachine = $AnimationTree.get("parameters/playback")
-	pass
-
-func _process(delta):
-	pass
 	
-func _physics_process(delta):
-	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	if direction != 0:
-		# speed up the player
-		velocity.x = lerp(velocity.x, direction * maxSpeed, acceleration)
-		switchSpriteDirection(direction)
+func _physics_process(delta: float):
+	var horizontalDirection = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	velocity = calculateMoveVelocity(horizontalDirection, delta)
+	
+	var isFalling = velocity.y > 0.0 and not is_on_floor()
+	var isJumping = Input.is_action_just_pressed("jump") and is_on_floor()
+	var isDoubleJumping = Input.is_action_just_pressed("jump") and isFalling
+	var isJumpCancelled = Input.is_action_just_released("jump") and velocity.y < minJumpHeight
+	var isIdling = is_on_floor() and is_zero_approx(velocity.x)
+	var isRunning = is_on_floor() and not is_zero_approx(velocity.x)
+	
+	# handle jumping
+	if isJumping:
+		jumpsMade += 1
+		velocity.y = maxJumpHeight
+	elif isDoubleJumping:
+		jumpsMade += 1
+		if jumpsMade <= maximumJumps:
+			velocity.y = doubleJumpHeight
+	elif isJumpCancelled:
+		velocity.y = minJumpHeight
+	elif isIdling or isRunning:
+		jumpsMade = 0
+	
+	# play animations	
+	if isJumping or isDoubleJumping:
+		$AnimationPlayer.play("Jump")
+	elif isRunning:
 		$AnimationPlayer.play("Run")
-#		stateMachine.travel("run")
+	elif isFalling:
+#		$AnimationPlayer.play("Fall")
+		pass
+	elif isIdling:
+		$AnimationPlayer.play("Idle")
+	
+	velocity = move_and_slide(velocity, upDiretion)
+	
+	switchSpriteDirection(horizontalDirection)
+	
+func calculateMoveVelocity(horizontalDirection, delta):
+	if horizontalDirection != 0:
+		# speed up the player
+		velocity.x = lerp(velocity.x, horizontalDirection * maxSpeed, acceleration)
 	else:
 		# slow down the player
 		velocity.x = lerp(velocity.x, 0, friction)
-		$AnimationPlayer.play("Idle")
-#		stateMachine.travel("idle")
-		
 	# aplly downwards gravity
 	velocity.y += gravity * delta
-		
-	velocity = move_and_slide(velocity, Vector2.UP)
 	
-	jump()
+	return velocity
 	
-	
-func jump():
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = maxJumpHeight
-		pass
-	elif Input.is_action_just_released("jump") && velocity.y < minJumpHeight:
-		velocity.y = minJumpHeight
-		pass
-	
-	
-func switchSpriteDirection(direction):
-	# switch direction
-	if direction > 0:
-		$Sprite.scale.x = 2
-	else:
-		$Sprite.scale.x = -2
+func switchSpriteDirection(horizontalDirection):
+	if horizontalDirection != 0:
+		if horizontalDirection > 0:
+			$Sprite.flip_h = false
+		else:
+			$Sprite.flip_h = true
