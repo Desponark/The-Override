073d0a5b7405e7a7c5d9f74d8caa8906a7f15e47
Paul Commerell (paul commerell <Paul Commerell (paul.commerell@gmail.com)>
2023-01-31 10:42:54 +0100

    - added _ignore variables to get rid of warnings - changed player enum states from lowercase to all UPPERCASE - changed health transfer code & cleaned up comments

--- Prototype2/assets/customNodes/HurtBox.gd
+++ Prototype2/assets/customNodes/HurtBox.gd
@@ -2,7 +2,7 @@ class_name HurtBox
 extends Area2D
 
 func _ready():
-	connect("area_entered", self, "_on_area_entered")
+	var _ignore = connect("area_entered", self, "_on_area_entered")
 	
 func _on_area_entered(area):
 	if area.has_method("damageHitTarget"):
--- Prototype2/assets/player/Dash/DashGhost.gd
+++ Prototype2/assets/player/Dash/DashGhost.gd
@@ -4,5 +4,5 @@ func _ready():
 	$Tween.interpolate_property(self, "modulate:a", 1.0, 0.0, 0.5, Tween.TRANS_QUART, Tween.EASE_OUT)
 	$Tween.start()
 
-func _on_Tween_tween_completed(object, key):
+func _on_Tween_tween_completed(_object, _key):
 	queue_free()
