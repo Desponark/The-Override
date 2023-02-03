12de2dd0995e08d76c6dab867cb4f18a117c8e2f
Paul Commerell <Paul Commerell@localhost>
2023-02-03 21:40:02 +0100

    - changed enemy size - changed health drop size - added light occluder to side walls - changed  socket healthbar scale and position - switch can now only be used if socket started charging - adjusted robot lightscale - fixed dash path - fixed player shader being bugged for some reason - changed esc to make game go to main menu instead of quitting the game directly - fixed spawners paths creating errors - adjusted wave defense level for smoother game introduction - added labels to wave defense level

--- Prototype2/assets/friendlies/Robot.gd
+++ Prototype2/assets/friendlies/Robot.gd
@@ -19,7 +19,7 @@ func _physics_process(_delta):
 			setRobotTransform(player.getRobotFollowPosition())
 
 func changeLightScale():
-	var lightScale = 3 * (getHealth() / getMaxHealth())
+	var lightScale = 2 * (getHealth() / getMaxHealth())
 	# stop lightscale from getting too small
 	if lightScale <= 0.4:
 		lightScale = 0.4
--- Prototype2/assets/player/Dash/Dash.gd
+++ Prototype2/assets/player/Dash/Dash.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var canDash = true
-var dashGhostScene = preload("res://assets/player/Dash/DashGhost.tscn")
+export(PackedScene) var dashGhostScene
 var sprite
 
 func startDash(givenSprite, duration):
