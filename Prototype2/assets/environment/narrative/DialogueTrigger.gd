63e11c4c92bc908df88adff9f4509a44cebbd8af
Felix Sch?fer <Felix Sch?fer@localhost>
2023-02-15 16:41:09 +0100

    - added dialogue files - changed dialogue trigger script with queue_free

--- Prototype2/assets/environment/narrative/DialogueTrigger.gd
+++ Prototype2/assets/environment/narrative/DialogueTrigger.gd
@@ -5,6 +5,7 @@ export(AudioStream) var dialougeStream
 func _on_DialogueTrigger_body_entered(body):
 	if body.has_method("playVoice"):
 		body.playVoice(dialougeStream)
+		queue_free()
 	
 	# disable collisions so voice cant be triggered again
 	collision_mask = 0
