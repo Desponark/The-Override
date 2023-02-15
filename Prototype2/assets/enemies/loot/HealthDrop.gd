e2df3ad80ffe703306c1b705fca88bbe79a2e920
Paul Commerell (paul commerell <129955348+Desponark@users.noreply.github.com>
2023-02-15 11:42:13 +0100

    - Health drops now fly into the health bar upon player touch - new health drop collision layer was added

--- Prototype2/assets/enemies/loot/HealthDrop.gd
+++ Prototype2/assets/enemies/loot/HealthDrop.gd
@@ -2,10 +2,20 @@ extends RigidBody2D
 
 
 export var healAmount = 25
+var player = null
 
-
+func _process(delta):
+	if player:
+		var tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+		tween.tween_property(self, "global_position", player.getHealthBarPosition(), 1)
+		
+#		linear_velocity = (player.getHealthBarPosition() - global_position)
 
 func _on_Area2D_body_entered(body):
-	if body.has_method("gainHealth"):
-		body.gainHealth(healAmount)
+	player = body
+	
+func _on_Area2D_area_entered(area):
+	if player and player.has_method("gainHealth"):
+		print("health absorbed: ", area)
+		player.gainHealth(healAmount)
 		queue_free()
