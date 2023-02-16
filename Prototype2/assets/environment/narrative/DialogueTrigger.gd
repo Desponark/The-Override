535e5f99707f85a3338a0b9d90ee974c5b09009b
Paul Commerell (paul commerell <129955348+Desponark@users.noreply.github.com>
2023-02-16 14:56:08 +0100

    - added speech bubble for showing and letting a text scroll up over a duration - added speech bubble to robot - adjusted dialogue triggers to trigger voice and text speech

--- Prototype2/assets/environment/narrative/DialogueTrigger.gd
+++ Prototype2/assets/environment/narrative/DialogueTrigger.gd
@@ -1,11 +1,10 @@
 extends Area2D
 
 export(AudioStream) var dialougeStream
+export(String) var dialogueText
+# TODO: replace dialoguText string type with text file for easier access and editing?
 
 func _on_DialogueTrigger_body_entered(body):
-	if body.has_method("playVoice"):
-		body.playVoice(dialougeStream)
+	if body.has_method("playSpeech"):
+		body.playSpeech(dialougeStream, dialogueText)
 		queue_free()
-	
-	# disable collisions so voice cant be triggered again
-	collision_mask = 0
