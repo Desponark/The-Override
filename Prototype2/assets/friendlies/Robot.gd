e66866c4a19b7578e946af1723ce113cf22f8621
Paul Commerell (paul commerell <129955348+Desponark@users.noreply.github.com>
2023-02-17 14:11:36 +0100

    - changed code how robot follows player now. laggy following should be eliminated - changed robot to kinematicbody2d - added follow speed multiplier and made it into an export var - made lightscale and minimum light scale multipliers into export vars

--- Prototype2/assets/friendlies/Robot.gd
+++ Prototype2/assets/friendlies/Robot.gd
@@ -1,13 +1,11 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 var isFollowingPlayer = false
 var player = null
+export var followSpeedMultiplier = 3
+export var lightScaleMultiplier = 0.5
+export var minimumLightScaleMultiplier = 0.3
 
-# TODO: think about changing from rigid body to node2d or sprite if physics are not needed for it
-
-func _integrate_forces(_state):
-	rotation_degrees = 0 # disable rotation... only useful if robot actually has collisions
-	
 # TODO: think about a better way to change light scale
 func _process(_delta):
 	changeLightScale()
@@ -16,24 +14,20 @@ func _physics_process(_delta):
 	if player != null:
 		if isFollowingPlayer:
 			$AnimationPlayer.play("idle")
-			setRobotTransform(player.getRobotFollowPosition())
+			move_and_slide((player.getRobotFollowPosition() - global_position) * followSpeedMultiplier)
 
 func changeLightScale():
-	var lightScale = 0.5 * (getHealth() / getMaxHealth())
+	var lightScale = lightScaleMultiplier * (getHealth() / getMaxHealth())
 	# stop lightscale from getting too small
-	if lightScale <= 0.4:
-		lightScale = 0.4
+	if lightScale <= minimumLightScaleMultiplier:
+		lightScale = minimumLightScaleMultiplier
 	$Light2D.set_texture_scale(lightScale)
-
-func setRobotTransform(position):
-	# TODO: recreating a new tween every frame seems bad? Creates slight stutter in some cases...
-	var tween = create_tween()
-	tween.tween_property(self, "global_position", position, 0.5)
 	
 func putRobotInSocket(position):
 	$AnimationPlayer.play("RESET")
 	isFollowingPlayer = false
-	setRobotTransform(position)
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", position, 0.5)
 
 func takeDamage(damage):
 	$VFXAnimationPlayer.play("hit")
@@ -57,16 +51,13 @@ func getPriority():
 	
 func playSpeech(dialougeStream, dialogueText, delay):
 	if !dialougeStream:
-		return
-	
+		return	
 	$VoicePlayer.stream = dialougeStream
-	$VoicePlayer.play()
-	
+	$VoicePlayer.play()	
 	$SpeechBubble.setup(dialogueText, dialougeStream.get_length())
 	$SpeechBubble.start()
 
 func _on_HealthBar_healthReachedZero():
-	#var _ignore = get_tree().reload_current_scene()
 	get_tree().change_scene("res://assets/ui/EndGameScreen/RobotLoseScreen.tscn")
 
 func _on_InteractionableBox_interacted(area):
