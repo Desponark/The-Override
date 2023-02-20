2f9f23ad3899579dcf3abe37cd0ce507e6c075fb
Paul Commerell (paul commerell <129955348+Desponark@users.noreply.github.com>
2023-02-20 17:04:44 +0100

    - removed hardcoded path to start screen - added pause to pause menu - disabled pause when exiting to start screen / main menu

--- Prototype2/assets/ui/menus/startMenu/StartScreen.gd
+++ Prototype2/assets/ui/menus/startMenu/StartScreen.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+export(PackedScene) var game
+
 func _on_NewGame_pressed():
-	get_tree().change_scene("res://Game.tscn")
+	get_tree().change_scene_to(game)
 
 func _on_QuitGame_pressed():
 	get_tree().quit()
