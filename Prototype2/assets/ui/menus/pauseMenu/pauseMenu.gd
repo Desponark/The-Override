4131dc3d3358217348ed2b9119b0cae73237ae97
Paul Commerell (paul commerell <Paul Commerell (paul.commerell@gmail.com)>
2023-02-20 17:04:44 +0100

    - removed hardcoded path to start screen - added pause to pause menu - disabled pause when exiting to start screen / main menu

--- Prototype2/assets/ui/menus/pauseMenu/pauseMenu.gd
+++ Prototype2/assets/ui/menus/pauseMenu/pauseMenu.gd
@@ -1,17 +1,29 @@
 extends Control
 
+var startScreen
+
+func setup(givenStartScreen):
+	startScreen = givenStartScreen
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().paused = false
+		hide()
+
+func start():
+	get_tree().paused = true
+	show()
+
 func _on_Continue_pressed():
 	get_tree().paused = false
 	hide()
 
-
 func _on_Options_pressed():
 	pass # Replace with function body.
 
-
 func _on_Main_Menu_pressed():
-	get_tree().change_scene("res://assets/ui/menus/startMenu/StartScreen.tscn")
-
+	get_tree().paused = false
+	get_tree().change_scene_to(startScreen)
 
 func _on_Quit_Game_pressed():
 	get_tree().quit()
