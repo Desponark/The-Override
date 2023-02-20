8f0ada8a96d23b28fdf3f2af587d2e9cc16e3876
Felix Sch?fer <Felix Sch?fer@localhost>
2023-02-20 14:11:17 +0100

    - added pause menu with functionality

--- Prototype2/assets/ui/menus/pauseMenu/pauseMenu.gd
+++ Prototype2/assets/ui/menus/pauseMenu/pauseMenu.gd
@@ -1,16 +1,17 @@
 extends Control
 
+func _on_Continue_pressed():
+	get_tree().paused = false
+	hide()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_Options_pressed():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Main_Menu_pressed():
+	get_tree().change_scene("res://assets/ui/menus/startMenu/StartScreen.tscn")
+
+
+func _on_Quit_Game_pressed():
+	get_tree().quit()
