c38b985716b0b279382072587ad0b16e8cac9e1b
Felix Sch?fer <Felix Sch?fer@localhost>
2023-02-21 13:14:08 +0100

    - added UI button sounds

--- Prototype2/assets/ui/menus/startMenu/StartScreen.gd
+++ Prototype2/assets/ui/menus/startMenu/StartScreen.gd
@@ -1,7 +1,25 @@
 extends Node2D
 
 func _on_NewGame_pressed():
+	$ButtonPressSound.play()
 	get_tree().change_scene("res://Game.tscn")
 
 func _on_QuitGame_pressed():
+	$ButtonPressSound.play()
 	get_tree().quit()
+
+
+func _on_Options_pressed():
+	$ButtonPressSound.play()
+
+
+func _on_NewGame_mouse_entered():
+	$ButtonHoverSound.play()
+
+
+func _on_Options_mouse_entered():
+	$ButtonHoverSound.play()
+
+
+func _on_QuitGame_mouse_entered():
+	$ButtonHoverSound.play()
