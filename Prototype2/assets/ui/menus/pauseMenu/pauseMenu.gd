a20628086b0c7a76120f2d6117348966ed8ca538
Paul Commerell (paul commerell <129955348+Desponark@users.noreply.github.com>
2023-02-21 17:55:51 +0100

    - fixed ability popup continue button having no code - added button hover and press sounds to pause menu

--- Prototype2/assets/ui/menus/pauseMenu/pauseMenu.gd
+++ Prototype2/assets/ui/menus/pauseMenu/pauseMenu.gd
@@ -15,15 +15,32 @@ func start():
 	show()
 
 func _on_Continue_pressed():
+	$ButtonPressSound.play()
 	get_tree().paused = false
 	hide()
 
 func _on_Options_pressed():
+	$ButtonPressSound.play()
 	pass # Replace with function body.
 
 func _on_Main_Menu_pressed():
+	$ButtonPressSound.play()
 	get_tree().paused = false
 	get_tree().change_scene_to(startScreen)
 
 func _on_Quit_Game_pressed():
+	$ButtonPressSound.play()
 	get_tree().quit()
+
+
+func _on_Continue_mouse_entered():
+	$ButtonHoverSound.play()
+
+func _on_Options_mouse_entered():
+	$ButtonHoverSound.play()
+
+func _on_Main_Menu_mouse_entered():
+	$ButtonHoverSound.play()
+
+func _on_Quit_Game_mouse_entered():
+	$ButtonHoverSound.play()
