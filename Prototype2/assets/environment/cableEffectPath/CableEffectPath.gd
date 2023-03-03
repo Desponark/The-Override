427790a7505875a06045c1e5d6b4ed3d7ca836ea
Paul Commerell (paul commerell <129955348+Desponark@users.noreply.github.com>
2023-03-03 10:15:29 +0100

    - changed script variable name for cableEffectPath

--- Prototype2/assets/environment/cableEffectPath/CableEffectPath.gd
+++ Prototype2/assets/environment/cableEffectPath/CableEffectPath.gd
@@ -1,7 +1,7 @@
 extends Path2D
 
 export var effectSpeed = 0.4
-export var spawnCooldown = 1.0
+export var effectFrequency = 1.0
 export(NodePath) var socketPath
 onready var socket = get_node_or_null(socketPath)
 
@@ -9,7 +9,7 @@ func _ready():
 	# TODO: think about how to connect to socket.
 	socket.connect("socketIsCharging", self, "socketIsCharging")
 	socket.connect("socketFullyCharged", self, "socketFullyCharged")
-	$Timer.wait_time = spawnCooldown
+	$Timer.wait_time = effectFrequency
 
 func socketIsCharging():
 	$Timer.start()
