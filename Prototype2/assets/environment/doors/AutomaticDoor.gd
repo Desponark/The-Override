70ff1fd3c8c7c99559976c81bca517bd9dd8c2b6
Paul Commerell (paul commerell <Paul Commerell (paul.commerell@gmail.com)>
2023-03-04 11:35:02 +0100

    - removed old levels - removed old door - adjusted automatic door

--- Prototype2/assets/environment/doors/AutomaticDoor.gd
+++ Prototype2/assets/environment/doors/AutomaticDoor.gd
@@ -1,8 +1,32 @@
-extends "res://assets/environment/doors/SpawnerDoor.gd"
+extends StaticBody2D
+
+
+export var isOpen = false
+var currentlyOpen = false
+var isLocked = false
 
 func _ready():
-	isLocked = true
+#	isLocked = true
 	$Sprite.self_modulate = Color(1,0,0)
+
+func _process(delta):
+	if isLocked:
+		return
+	
+	var bodies = $Area2D.get_overlapping_bodies()
+	if bodies.size() > 0:
+		isOpen = true
+	else:
+		isOpen = false
+		
+	if !currentlyOpen and isOpen:
+		$AnimationPlayer.play("open")
+		$DoorOpeningSound.play()
+		currentlyOpen = true
+	elif currentlyOpen and !isOpen:
+		$AnimationPlayer.play_backwards("open")
+		$DoorClosingSound.play()
+		currentlyOpen = false
 
 func lock(seconds):
 	isLocked = true
